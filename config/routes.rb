6610569b7837f1a320fc165129cd48b5f3353784
Saori Demizu <sa.sssky717@gmail.com>
2019-03-18 00:41:52 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'message#index'
   get 'chat-space' => "message#index"
-  resources :users, only: [:edit,:update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:create, :new, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
