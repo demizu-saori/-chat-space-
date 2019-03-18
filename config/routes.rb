f3d32fb921b7038fd7e2be1c3250db4985b90d1e
Saori Demizu <sa.sssky717@gmail.com>
2019-03-18 23:28:43 +0900

    Change root group#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'message#index'
-  get 'chat-space' => "message#index"
+  root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:create, :new, :edit, :update] do
     resources :messages, only: [:index, :create]
